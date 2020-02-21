0e4cb4579d6ac9aca18ca24f3c10ee5e85aba18e
Thierry Touin <thierry.touin@gfi.fr>
2020-02-21 10:14:33 +0100

    service worker

--- main.js
+++ main.js
@@ -1,24 +1,3 @@
-// console.log('hello depuis main');
-// const technosDiv = document.querySelector('#technos');
-
-// let technos = [
-//     {id: 1, name: 'Angular', description: 'le framework front-end', url: 'https://angular.io/'},
-//     {id: 2, name: 'Node', description: 'JavaScript côté backe-end', url: 'https://nodejs.org/en/'},
-//     {id: 3, name: 'MongoDB', description: 'la célèbre base noSQL', url: 'https://www.mongodb.com/'},
-//     {id: 4, name: 'PWA', description: 'rendre vos applications ++', url: 'https://developer.mozilla.org/en-US/Apps/Progressive'}
-// ];
-
-// function loadTechnologies(technos) {
-//     const allTechnos = technos
-//         .map(t => `<div><b>${t.name}</b> ${t.description} - site officiel </div>`)
-//         .join('');
-
-//     technosDiv.innerHTML = allTechnos; 
-// }
-
-// loadTechnologies(technos);
-
-
 console.log('hello depuis main');
 const technosDiv = document.querySelector('#technos');
 
@@ -37,3 +16,9 @@ function loadTechnologies(technos) {
 }
 
 loadTechnologies(technos);
+
+
+if (navigator.serviceWorker) {
+    navigator.serviceWorker.register('sw.js')
+        .catch(err => console.log(err));
+}
--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
-console.log("service worker");
+console.log("hello depuis le service worker");
+
+self.addEventListener('install', evt => {
+    console.log('install evt : ' , evt);
+})
+
+self.addEventListener('activate', evt => {
+    console.log('activate evt : ', evt);
+})
+
+self.addEventListener('fetch', evt => {
+    console.log('fetch evt sur url : ', evt.request.url);
+})
